cbabf57d00f2889d2cc8e3f0d012e1ee68c9268b
dondish <odedshapira1@gmail.com>
2020-06-25 17:45:24 +0300

    Added integration test to ping.

--- src/test/kotlin/command/CommandIntegrationTests.kt
+++ src/test/kotlin/command/CommandIntegrationTests.kt
@@ -3,12 +3,16 @@ package command
 import dev.augu.nino.butterfly.command.Command
 import dev.augu.nino.butterfly.command.CommandContext
 import dev.augu.nino.butterfly.util.edit
+import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import io.mockk.clearAllMocks
-import io.mockk.spyk
+import io.mockk.*
+import kotlinx.coroutines.test.runBlockingTest
+import net.dv8tion.jda.api.entities.Message
+import java.time.Duration
+import java.time.OffsetDateTime
 import java.time.temporal.ChronoUnit
 
 class CommandIntegrationTests : DescribeSpec({
@@ -33,6 +37,31 @@ class CommandIntegrationTests : DescribeSpec({
             cmd.aliases[0].shouldBe("pong")
             cmd.aliases.shouldHaveSize(1)
             cmd.guildOnly.shouldBeTrue()
+        }
+
+        it("Integration Test") {
+            val ctx = mockk<CommandContext>(relaxed = true)
+            val msg = mockk<Message>(relaxed = true)
+            val time = OffsetDateTime.now()
+
+            every { ctx.client.gatewayPing } returns 5
+            every { ctx.message.timeCreated } returns time
+            every { msg.timeCreated } returns time + Duration.of(5, ChronoUnit.SECONDS)
+            coEvery { ctx.reply(any<CharSequence>()) } coAnswers {
+                msg
+            }
+            mockkStatic("dev.augu.nino.butterfly.util.MessageExtensions")
+            coEvery { msg.edit(any<CharSequence>()) } returns msg
+
+            shouldNotThrow<Exception> {
+                runBlockingTest {
+                    cmd.execute(ctx)
+                }
+            }
+
+            coVerify(exactly = 1) { ctx.reply("Calculating...") }
+            coVerify(exactly = 1) { msg.edit("Ping: 5000ms | Websocket: 5ms") }
+
         }
     }
 })
