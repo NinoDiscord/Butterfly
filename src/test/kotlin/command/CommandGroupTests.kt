8ec733515304221d2e1a261b3074f2b6b57d55f3
dondish <odedshapira1@gmail.com>
2020-07-05 16:52:00 +0300

    Finished CommandGroupTests

--- src/test/kotlin/command/CommandGroupTests.kt
+++ src/test/kotlin/command/CommandGroupTests.kt
@@ -15,7 +15,6 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.*
 
 class CommandGroupTests : DescribeSpec({
-    // TODO: Add CommandGroup tests
     describe("CommandGroup Tests") {
         it("should throw an error if there are no suitable commands") {
             val cmd = spyk(object : Command("example", "generic") {
@@ -62,6 +61,52 @@ class CommandGroupTests : DescribeSpec({
 
             every { ctx.prefix } returns "!"
             every { ctx.args } returns arrayOf("example")
+            every { message.isFromGuild } returns true
+            every { ctx.message } returns message
+            every { message.member } returns member
+            every { message.author } returns author
+            every { message.textChannel } returns channel
+            every { message.channel } returns channel
+            every { message.guild } returns guild
+            every { guild.selfMember } returns meMember
+            every { member.hasPermission(any<GuildChannel>(), any<Collection<Permission>>()) } returns true
+            every { meMember.hasPermission(any<GuildChannel>(), any<Collection<Permission>>()) } returns true
+            every { ctx.command } returns group
+            every { client.selfUser } returns meUser
+            every { ctx.client } returns client
+            mockkStatic("dev.augu.nino.butterfly.util.MessageExtensions")
+            coEvery { message.reply(any<CharSequence>()) } returns message
+
+            shouldNotThrow<CommandException> {
+                runBlocking {
+                    group.execute(ctx)
+                }
+            }
+
+            coVerify(exactly = 1) {
+                cmd.execute(any())
+            }
+        }
+
+        it("should run the default command if no command is found") {
+            val cmd = spyk(object : Command("example", "generic") {
+                override suspend fun execute(ctx: CommandContext) {
+                    ctx.reply("example")
+                }
+            })
+            val group = CommandGroup("test", arrayOf(cmd).asList(), "generic", defaultCommand = cmd)
+            val ctx = mockk<CommandContext>()
+            val message = mockk<Message>()
+            val client = mockk<ButterflyClient>()
+            val author = mockk<User>()
+            val meUser = mockk<SelfUser>()
+            val member = mockk<Member>()
+            val meMember = mockk<Member>()
+            val channel = mockk<TextChannel>()
+            val guild = mockk<Guild>()
+
+            every { ctx.prefix } returns "!"
+            every { ctx.args } returns arrayOf("yee")
             every { message.isFromGuild } returns true
             every { ctx.message } returns message
             every { message.member } returns member
